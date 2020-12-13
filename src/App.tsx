fa556def230d05830d86f7f1e424066ac0d5669b
Lauro F. Müller <contact@lauromueller.com>
2020-12-13 08:11:34 +0100

    feat: integrate material-ui into project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button } from '@material-ui/core';
 
-function App() {
+const GreetingText = (props: any) => {
+  const greetingText = props.timeOfDay === 'morning' ? 'Good morning' : 'Good evening';
+
+  return <h1>
+    {greetingText}
+  </h1>
+}
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GreetingText timeOfDay='morning' />
+      <Button color='primary' variant='contained'>
+        Some text
+      </Button>
     </div>
   );
 }
