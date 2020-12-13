fe72f4bb0a15f2ffa78df6669d5cda808a18134f
Lauro F. Müller <contact@lauromueller.com>
2020-12-13 08:45:55 +0100

    feat: add eslint, prettier, husky, lint-staged

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,14 @@ import React from 'react';
 import './App.css';
 import { Button } from '@material-ui/core';
 
-const GreetingText = (props: any) => {
-  const greetingText = props.timeOfDay === 'morning' ? 'Good morning' : 'Good evening';
-
-  return <h1>
-    {greetingText}
-  </h1>
-}
-
 const App = () => {
   return (
-    <div className="App">
-      <GreetingText timeOfDay='morning' />
+    <div className='App'>
       <Button color='primary' variant='contained'>
         Some text
       </Button>
     </div>
   );
-}
+};
 
 export default App;
