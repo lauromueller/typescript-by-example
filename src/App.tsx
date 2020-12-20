274306fc9926c5656028c7e5a87964e98e05f335
Lauro F. Müller <contact@lauromueller.com>
2020-12-20 10:35:37 +0100

    feat: add Product and Tag data models + ProductComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,30 @@
 import React from 'react';
 import './App.css';
 import { Button } from '@material-ui/core';
+import { Product } from './data-models';
+
+const myFirstProduct: Product = {
+  id: '1',
+  title: 'My First Product',
+  price: 50,
+  tags: [],
+  description: 'This is my first product',
+};
+
+type ProductComponentProps = {
+  product: Product;
+};
+
+const ProductComponent = (props: ProductComponentProps) => {
+  const { product } = props;
+
+  return (
+    <div>
+      <h1>{product.title}</h1>
+      <p>{product.description}</p>
+    </div>
+  );
+};
 
 const App = () => {
   return (
@@ -8,6 +32,7 @@ const App = () => {
       <Button color='primary' variant='contained'>
         Some text
       </Button>
+      <ProductComponent product={myFirstProduct} />
     </div>
   );
 };
---
+++ src/data-models/product.ts
@@ -0,0 +1,10 @@
+import { Tag } from './tag';
+
+export type Product = {
+  id: string;
+  title: string;
+  price: number;
+  image?: string;
+  tags: Tag[];
+  description: string;
+};
